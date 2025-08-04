c19d77e51ae4ba817c1bbbe9d2dfc3b41f1382e6
RenSkywalker <80165919+RenSkywalker@users.noreply.github.com>
2025-08-04 13:03:23 -0300

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#ffffff"
+        "value": "#A020F0"
       },
       "shape": {
         "type": "circle",
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ffffff",
+        "color": "#A020F0",
         "opacity": 0.4,
         "width": 1
       },
